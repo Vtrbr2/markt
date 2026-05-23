3e88029e338dd2f9f1e7249bc1dcb3735ab0871f
cryptitys <vrodriguespereira691@gmail.com>
2026-05-22 22:22:35 -0300

    Update create-category.dto.ts

--- apps/backend/src/modules/categories/dto/create-category.dto.ts
+++ apps/backend/src/modules/categories/dto/create-category.dto.ts
@@ -1,1 +1,17 @@
+import { IsString, IsOptional, IsUUID } from 'class-validator';
 
+export class CreateCategoryDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  slug: string;
+
+  @IsOptional()
+  @IsUUID()
+  parentId?: string;
+
+  @IsOptional()
+  @IsString()
+  iconUrl?: string;
+}
