3a1842d4ec0904265b918c89701bc3fa451dadf1
cryptitys <vrodriguespereira691@gmail.com>
2026-05-22 22:56:42 -0300

    Update main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,5 +10,13 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors(); // Configurar melhor em produção
   await app.listen(3001);
+
+ // Servir arquivos estáticos da pasta uploads
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+  await app.listen(3001);
+  }
 }
 bootstrap();
